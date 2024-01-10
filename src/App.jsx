c1965978fe0c4a69a1f19ceb6eb78ef9cdcca712
Amanuel <mohaaman922@gmail.com>
2024-01-10 16:35:07 +0300

    add graphics

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
+import { Home, About, Projects, Contact } from "./pages";
 import Navbar from './components/Navbar';
 const App = () => {
   return (
@@ -7,13 +8,12 @@ const App = () => {
           <Router>
               <Navbar />
                   <Routes>
-                      <Route path='/' element={'Home'} />
-                      <Route path='/about' element={'About'} />
-                      <Route path='/projects' element={'Projects'} />
-                      <Route path='/contact' element={'Contact'} />
-                  </Routes>
-              
-          </Router>
+                      <Route path='/' element={<Home/>} />
+          <Route path='/about' element={<About/>} />
+          <Route path='/projects' element={<Projects/>} />
+          <Route path='/contact' element={<Contact/>} />             
+          </Routes>
+         </Router>
    </main>
   )
 }
